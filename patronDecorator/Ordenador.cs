0034df6c36e6c1f1ad69d96c224b51e0ff0d79ec
jorge nanez <kalem_rm@hotmail.es>
2017-04-07 17:55:47 -0500

    patron de diseño decorator

---
+++ patronDecorator/Ordenador.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patronDecorator
+{
+            public abstract class Ordenador
+        {
+            // Atributo común a todos los objetos que heredarán de esta clase
+            protected string descripcion = "Ordenador genérico";
+
+            // Método no abstracto que devolverá el contenido de la descripción
+            // Se declara como virtual para que pueda sustituirse en las clases derivadas
+            public virtual string Descripcion()
+            {
+                return descripcion;
+            }
+
+            // Métodos abstractos
+            public abstract string Accesorio();
+            public abstract int Precio();
+        }
+
+    public class CompBasic : Ordenador
+    {
+        public CompBasic()
+        {
+            descripcion = "Computador Basico";
+        }
+
+        // Funcionalidad básica
+        public override string Accesorio()
+        {
+             return "Mouse";
+        }
+
+        // Funcionalidad básica
+        public override int Precio()
+        {
+            return 4500;
+        }
+    }
+
+    public class Toshiba : Ordenador
+    {
+        public Toshiba()
+        {
+            descripcion = "Toshiba C-40";
+        }
+
+        public override string Accesorio()
+        {
+            return "Bocinas";
+        }
+
+        public override int Precio()
+        {
+            return 7000;
+        }
+    }
+
+    public abstract class Decorator : Ordenador
+    {
+        // Declaramos el método como abstracto para que todos los decoradores lo
+        // reimplementen.
+        public override abstract string Descripcion();
+    }
+   public class Impresora : Decorator
+    {
+        // Instancia de la clase ordenador
+        private Ordenador computador;
+
+        // Constructor que recibe el ordenador que encapsulará el decorator
+        public Impresora(Ordenador computador)
+        {
+            this.computador = computador;
+        }
+
+        // Los métodos utilizan la información del objeto encapsulado y le
+        // incorporan su propia funcionalidad.
+        public override string Descripcion()
+        {
+            return computador.Descripcion() + " Impresora";
+        }
+        
+        public override string Accesorio()
+        {
+            return computador.Accesorio() + " + Tinta";
+        }
+
+        public override int Precio()
+        {
+            return computador.Precio() + 500;
+        }
+    }
+
+    public class Disipador : Decorator
+    {
+        
+        private Ordenador computador;
+
+        
+        public Disipador(Ordenador computador)
+        {
+            this.computador = computador;
+        }
+
+        public override string Descripcion()
+        {
+            return computador.Descripcion() + " Disipador";
+        }
+
+        public override string Accesorio()
+        {
+            return computador.Accesorio() + " + bateria";
+        }
+
+        public override int Precio()
+        {
+            return computador.Precio() + 200;
+        }
+    }
+}
---
+++ patronDecorator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patronDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ordenador compBasico = new CompBasic();
+            Ordenador toshiba = new Toshiba();
+            
+            //Mostramos los parametros basicos
+            MostrarCaracteristicas(compBasico);
+            MostrarCaracteristicas(toshiba);
+
+            //mostramos los parametros mas los decoradores
+            compBasico = new Impresora(compBasico);
+            MostrarCaracteristicas(compBasico);
+
+            toshiba = new Disipador(toshiba);
+            MostrarCaracteristicas(toshiba);
+
+            toshiba = new Impresora(toshiba);
+            MostrarCaracteristicas(toshiba);
+
+            Console.ReadKey();
+        }
+
+        private static void MostrarCaracteristicas(Ordenador v)
+        {
+            Console.WriteLine(string.Format("{0}\n\tAccesorio Incluido: {1} \n\tPrecio del producto: {2} \n",
+                                            v.Descripcion(),
+                                            v.Accesorio(),
+                                            v.Precio()));
+        }
+    }
+}
